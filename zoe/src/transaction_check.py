2e6a91c63945ed544e8f59da699464a581f50774
Mateusz Kopec <kop.mateusz@gmail.com>
2018-05-11 19:46:53 +0200

    fixes

--- zoe/src/transaction_check.py
+++ zoe/src/transaction_check.py
@@ -6,21 +6,24 @@ depth = input()
 print "import './root_check{}.code' as root_check".format(depth)
 print " "
 print "\\\\ i256 input_invalidator, i256 root, i32 out_value, i256 change_commitment"
-private_input_path = (", ").join(["private input_path_{}".format(i) for i in range(0, depth)])
-input_path = (", ").join(["input_path_{}".format(i) for i in range(0, depth)])
+left_private_input_path = (", ").join(["private left_input_path_{}".format(i) for i in range(0, depth)])
+left_input_path = (", ").join(["left_input_path_{}".format(i) for i in range(0, depth)])
+right_private_input_path = (", ").join(["private right_input_path_{}".format(i) for i in range(0, depth)])
+right_input_path = (", ").join(["right_input_path_{}".format(i) for i in range(0, depth)])
+
 print "def main(input_invalidator, root, out_commitment, change_commitment, \
     private input_value, private input_id, private input_private_key, private input_public_key, \
-    private input_commitment, {}, \
+    private input_commitment, {}, {}, \
     private change_value, private change_id, private change_public_key, \
     private out_value, private out_id, private out_public_key\
     ):" \
-    .format(private_input_path)
+    .format(left_private_input_path, right_private_input_path)
 #TODO everything has to be private
 print "  \\\\Check input coin is correct"
 print "  commitment_check(input_value, input_commitment, input_id, input_public_key) == 1"
 print "  public_key_check(input_public_key, input_private_key) == 1"
 print "  invalidator_check(input_invalidator, input_private_key, input_id) == 1"
-print "  root_check(root, input_commitment, {}) == 1".format(input_path)
+print "  root_check(root, input_commitment, {}, {}) == 1".format(left_input_path, right_input_path)
 print "  "
 print "  \\\\Check if output is correct"
 print "  input_value == out_value + change_value"
