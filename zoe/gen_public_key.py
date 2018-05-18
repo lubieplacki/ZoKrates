eb00c6872ac1de04607ba8ca4dae4d3c52f0462a
Mateusz Kopec <kopmateusz@gmail.com>
2018-05-18 17:46:03 +0200

    gen_public_key

---
+++ zoe/gen_public_key.py
@@ -0,0 +1,43 @@
+from subprocess import call
+import json
+secret_key = raw_input("Please provide 32-character secret key:\n")
+secret_key = secret_key[0:32]
+
+def bits_to_int(bits):
+    res = 0
+    for i in range(0, len(bits)):
+        res = res * 2 + bits[len(bits) - 1 - i]
+    return res
+
+print secret_key
+secret_key_bits = []
+for i in range(0, 32):
+    x = 0
+    if i < len(secret_key):
+        x = ord(secret_key[i])
+    for j in range(0, 8):
+        secret_key_bits.append(x % 2)
+        x = x / 2
+
+params = "{} {}".format(" ".join([str(secret_key_bits[i]) for i in range(0, 256)]), " ".join(["0" for i in range(0, 256)]))
+call("../target/release/zokrates compute-witness -a {} > public_key.witness".format(params))
+print "Your secret key:"
+secret_int = bits_to_int(secret_key_bits)
+print secret_int
+
+with open("secret.key","w") as sk_file:
+    sk_file.write(str(secret_int))
+with open('public_key.witness', 'r') as witness_file:
+    witness_output_json = (witness_file.read())[9:]
+
+witness_output = json.loads(witness_output_json)
+public_key_bits = []
+for i in range(0, 256):
+    public_key_bits.append(int(witness_output["~out_{}".format(i)]))
+
+public_int = bits_to_int(public_key_bits)
+
+print "Your public key:"
+print public_int
+with open("public.key","w") as pb_file:
+    pb_file.write(str(public_int))
