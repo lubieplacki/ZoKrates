23640ed859a9b092b545a1e79462803f47fc594f
Mateusz Kopec <kopmateusz@gmail.com>
2018-06-13 19:01:33 +0200

    nit

--- zoe/proofs/sha256/sha256.py
+++ zoe/proofs/sha256/sha256.py
@@ -4,7 +4,7 @@ for x in range(511,-1,-1):
 input_par = ", ".join(input_par)
 output_par = []
 for x in range(255,-1,-1):
-    input_par.append("o" + str(x))
+    output_par.append("o" + str(x))
 output_par = ", ".join(output_par)
 print "def main({}):".format(input_par)
 print "  {} = sha256libsnark({})".format(output_par, input_par)
