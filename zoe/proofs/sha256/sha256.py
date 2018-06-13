a0c958136b2f2712979238203178d80a8b5ff329
Mateusz Kopec <kopmateusz@gmail.com>
2018-06-13 18:56:56 +0200

    fixed sha256

--- zoe/proofs/sha256/sha256.py
+++ zoe/proofs/sha256/sha256.py
@@ -2,5 +2,10 @@ input_par = []
 for x in range(511,-1,-1):
     input_par.append("i" + str(x))
 input_par = ", ".join(input_par)
+output_par = []
+for x in range(255,-1,-1):
+    input_par.append("o" + str(x))
+output_par = ", ".join(output_par)
 print "def main({}):".format(input_par)
-print "  return sha256libsnark({})".format(input_par)
+print "  {} = sha256libsnark({})".format(output_par, input_par)
+print "  return {}".format(output_par)
