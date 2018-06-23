681d02a899567c30b0fb944e5fa9b173ec1f2e36
Mateusz Kopec <kopmateusz@gmail.com>
2018-06-23 11:57:09 +0200

    test

--- zoe/tests/test.py
+++ zoe/tests/test.py
@@ -34,13 +34,13 @@ in_commitment = commitments[1]['commit']
 rsa_public_key_change = rsa_public_key
 public_key_change = public_key
 out_value = 5
-result = transaction(w3, manager, public_key, secret_key, 5, out_pk, rsa_public_key_out, rsa_public_key, in_value, in_commitment, in_secret)
+result = transaction(w3, manager, public_key, secret_key, 5, out_pk, rsa_public_key_out, rsa_public_key, in_value, in_commitment, in_secret, commitments[1]['id'])
 available_commitments(manager, secret_key, public_key, rsa_private_key)
 
 w3.eth.getBalance(w3.eth.accounts[0])
 w3.eth.getBalance(manager.address)
 
-transaction(w3, manager, public_key, secret_key, 1, out_pk, rsa_public_key_out, rsa_public_key, commitments[0]['v'], commitments[0]['commit'], commitments[0]['s'])
+transaction(w3, manager, public_key, secret_key, 1, out_pk, rsa_public_key_out, rsa_public_key, commitments[0]['v'], commitments[0]['commit'], commitments[0]['s'], commitments[0]['id'])
 available_commitments(manager, secret_key, public_key, rsa_private_key)
 
 w3.eth.getBalance(w3.eth.accounts[0])
@@ -56,7 +56,7 @@ in_commitment = commitments[0]['commit']
 rsa_public_key_change = rsa_public_key_out
 public_key_change = public_key
 out_value = 3
-withdraw(w3, manager, out_pk, out_sk, out_value, commitsSecondGuy[0]['v'], commitsSecondGuy[0]['commit'], commitsSecondGuy[0]['s'], rsa_public_key_out)
+withdraw(w3, manager, out_pk, out_sk, out_value, commitsSecondGuy[0]['v'], commitsSecondGuy[0]['commit'], commitsSecondGuy[0]['s'], rsa_public_key_out, commitsSecondGuy[0]['id'])
 
 
 w3.eth.getBalance(w3.eth.accounts[1])
